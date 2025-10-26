d566c7d6e68eb263c759134c8e527d9de63045f6
Snigdh008 <leendsoggicial@gmail.com>
2025-10-26 17:19:25 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
 // Game State
 const gameState = {
